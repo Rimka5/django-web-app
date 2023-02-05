69bd5917134593b4ba8dbebffb6f491c346b5fe3
Rimka5 <karim.tensaout@gmail.com>
2023-02-05 22:21:39 +0100

    création de l'admin

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=100)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    
+    class Band(models.Model):
+
+        class Genre(models.TextChoices):
+            HIP_HOP = 'HH'
+            SYNTH_POP = 'SP'
+            ALTERNATIVE_ROCK = 'AR'
+
+        genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    def __str__(self):
+            return f'{self.name}'
