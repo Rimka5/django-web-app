59104d92ca16300c0f160d93896493ccdd76f47e
Rimka5 <karim.tensaout@gmail.com>
2023-02-06 09:56:49 +0100

    init

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from listings.models import Band
+from listings.models import Band, Listing
 
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name', 'year_formed', 'genre')    
 admin.site.register(Band, BandAdmin)
+admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'band')
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+            HIP_HOP = 'HH'
+            SYNTH_POP = 'SP'
+            ALTERNATIVE_ROCK = 'AR'
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    def __str__(self):
+            return f'{self.name}'
+        
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(max_length=50)
     biography = models.fields.CharField(max_length=100)
@@ -11,13 +19,20 @@ class Band(models.Model):
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
     
-    class Band(models.Model):
-
-        class Genre(models.TextChoices):
-            HIP_HOP = 'HH'
-            SYNTH_POP = 'SP'
-            ALTERNATIVE_ROCK = 'AR'
-
-        genre = models.fields.CharField(choices=Genre.choices, max_length=5)
-    def __str__(self):
-            return f'{self.name}'
+    class listing (models.Model):
+        class ListingType (models.TextChoices):
+            RECORDS = 'R'
+            CLOTHING = 'C'
+            POSTERS = 'P'
+            MISC = 'M'
+        
+        title = models.fields.CharField(max_length=100)
+        description = models.fields.CharField(max_length=1000)
+        sold = models.fields.models.BooleanField(default=False)
+        year = models.fields.IntegerField(
+            null=True,
+            validators=[MinValueValidator(1900),
+                        MaxValueValidator(2021)]
+        )
+        type = models.fields.CharField(choices=ListingType.choices, max_length=5)
+band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
